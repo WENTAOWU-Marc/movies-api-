76100274adc82f19390901329a0d5853f7f17664
WENTAO WU <872257263@qq.com>
2020-12-13 22:46:37 +0000

    Week 9 User Favourites

--- api/users/index.js
+++ api/users/index.js
@@ -13,6 +13,7 @@ router.post('/', (req, res ,next) => {
     // eslint-disable-next-line no-unused-vars
     User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
 });
+
 // Update a user
 router.put('/:id',  (req, res ,next) => {
     if (req.body._id) delete req.body._id;
@@ -23,4 +24,28 @@ router.put('/:id',  (req, res ,next) => {
     })
     .then(user => res.json(200, user)).catch(next);
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
+
+  router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
 export default router;
