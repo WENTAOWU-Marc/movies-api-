83dc7c68906921b096f0000a2fa65ff487af9bbe
WENTAO WU <872257263@qq.com>
2020-12-13 19:35:37 +0000

    Week 9 Error HAndling

--- api/movies/index.js
+++ api/movies/index.js
@@ -4,19 +4,27 @@ import {
 } from '../tmdb-api';
 
 const router = express.Router();
-
+ 
+// eslint-disable-next-line no-unused-vars
 router.get('/', (req, res,next) => {
-  getMovies().then(movies => res.status(200).send(movies));
+  getMovies().then(movies => res.status(200).send(movies))
+  .catch((error) => next(error));
 });
 
+// eslint-disable-next-line no-unused-vars
 router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovie(id).then(movie => res.status(200).send(movie));
+  getMovie(id).then(movie => res.status(200).send(movie))
+  .catch((error) => next(error));
+    
 });
 
+// eslint-disable-next-line no-unused-vars
 router.get('/:id/reviews', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
+  getMovieReviews(id)
+  .then(reviews => res.status(200).send(reviews))
+  .catch((error) => next(error));
 });
 
 export default router;
--- index.js
+++ index.js
@@ -7,12 +7,25 @@ dotenv.config();
 
 const app = express();
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT;
+
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  // eslint-disable-next-line no-undef
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
+app.use(errHandler);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
